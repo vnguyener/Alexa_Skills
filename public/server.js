540e4f34fa526a30f8fd9570fe81235786555a26
vnguyener <its.vnguyen@gmail.com>
2016-07-06 15:19:26 -0500

    cleaning

--- public/server.js
+++ public/server.js
@@ -4,11 +4,10 @@ AlexaAppServer.start({
 	// Use preRequest to load user data on each request and add it to the request json.
 	// In reality, this data would come from a db or files, etc.
 	preRequest: function(json, req, res) {
-		console.log('preRequest fired');
-		json.userDetails = {'name': 'Vu Nguyen'};
+		console.log('trying');
 	},
 	// Add a dummy attribute to the response
 	postRequest: function(json, req, res) {
-		json.dummy = 'text';
+		console.log('done trying');
 	}
 });
