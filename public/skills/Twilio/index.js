bc2d55baf0cb195278284530206eaf7da8487565
vnguyener <its.vnguyen@gmail.com>
2016-07-06 14:59:41 -0500

    added twilio base to skills

--- public/skills/Twilio/index.js
+++ public/skills/Twilio/index.js
@@ -1,22 +1,56 @@
 "use strict"
 
 const alexa = require('alexa-app');
-const app = new alexa.app('hey-twilio');
-const twilio = require('twilio')('AC316319c0039bfb33608b7296eb07b9f2', '1e84e0acdae2699f5ae02bbc97bc8a62');
+const app = new alexa.app('heytwilio');
+const twilio = require('./services/twilio.service');
+
 
 module.change_code = 1;
 
 app.launch((req, res) => {
-   const prompt = "";
-   const reprompt = "";
-   
+    const prompt = "Hey, welcome to Hey Twilio! Who do you wanna shoot a text to?";
+    const reprompt = "Still there?";
+
     res.say(prompt).reprompt(reprompt).shouldEndSession(false);
 });
 
 app.intent('text', {
-    'slots': {'message': 'LITERAL'},
-    'utterances': ['{message}']
+    'slots': { 'message': 'LITERAL' },
+    'utterances': ['say {message}', 'send {message}']
 }, (req, res) => {
+
     let message = req.slot('message');
-    
+    let options = {
+        account_sid: '',
+        auth_token: '',
+        from: ''
+    }
+
+    let handler = function (err, res) {
+        if (err) {
+            console.log(err);
+        }
+    }
+
+    if (message) {
+        //res.say('Got it, who you do you want me to send it to?');
+        // grab name from session
+        try {
+            res.say('Got it. Sending now.');
+            twilio.sendTextMessage(options, '', message, handler);
+            res.shouldEndSession(true);
+        } catch (err) {
+            res.say('There was an error with the service call saying');
+            res.say(err.mesage);
+            res.say('Let me try again.');
+            res.shouldEndSession(false);
+        }
+
+    } else {
+        res.say('Sorry, I didn\'t get that. Could you tell me again?');
+        res.shouldEndSession(false);
+    }
+
 });
+
+module.exports = app;
--- public/skills/Twilio/services/twilio.service.js
+++ public/skills/Twilio/services/twilio.service.js
@@ -1,12 +1,40 @@
-const twilio = require('twilio')('AC316319c0039bfb33608b7296eb07b9f2', '1e84e0acdae2699f5ae02bbc97bc8a62');
+"use strict"
+
+const twilio = require('twilio');
+
 
 
 let TwilioService = function() {
-    
-    
-    return {
-        
+    let initialize = function(options) {
+        options = options || {};
+
+        try {
+            return new twilio.RestClient(options.account_sid, options.auth_token);
+        } catch(err) {
+            console.log(err);
+        }
+    }
+
+    let sendTextMessage = function(options, to, body, callback) {
+        let client = initialize(options);
+
+        client.sendMessage({
+            to: to || '',
+            from: options.from || '',
+            body: body || 'ahoy'
+        }, (err, res) => {
+            if (!err) {
+                console.log('Message send from: ' + res.from);
+                callback(null, res);
+            } else {
+                callback(err, res);
+            }
+        });
     };
+
+    return {
+        sendTextMessage: sendTextMessage,
+    }
 };
 
 module.exports = TwilioService();
